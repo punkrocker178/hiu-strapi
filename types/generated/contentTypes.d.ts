f36994ba614b56bf3c6d53fd8870cb8d490520cf
Hieu Le <ngochieu.le1708@gmail.com>
2025-03-21 18:10:02 +0700

    Update description to rich text

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -426,7 +426,7 @@ export interface ApiProductProduct extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    description: Schema.Attribute.Text;
+    description: Schema.Attribute.Blocks;
     images: Schema.Attribute.Media<
       'images' | 'files' | 'videos' | 'audios',
       true
